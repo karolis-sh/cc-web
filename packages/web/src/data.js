55683d9bf79e0691e466f2091d0d45713cf04a3c
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-01-01 16:06:35 +0200

    Add 'contact us' section

--- packages/web/src/data.js
+++ packages/web/src/data.js
@@ -5,12 +5,14 @@ if (['esc', 'statrem'].indexOf(target) === -1) {
 
 const data = {
   esc: {
+    $key: 'esc',
     url: 'http://esconstruction.eu',
     urlName: 'esconstruction.eu',
     startYear: 2017,
     companyName: 'Eco Scandinavian Construction',
   },
   statrem: {
+    $key: 'statrem',
     url: 'http://statrem.eu',
     urlName: 'statrem.eu',
     startYear: 2016,
--- packages/web/src/pages/contacts.js
+++ packages/web/src/pages/contacts.js
@@ -1,16 +1,85 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import graphql from 'graphql';
 import { FormattedMessage } from 'react-intl';
+import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
 
+import contentData from '../data';
+import { media } from '../style';
+import { CurrentLocale } from '../i18n/components';
 import { ContentTitle } from '../components';
 
-function ContactsPage() {
+const Contacts = styled.div`
+  ${media.notSmall`
+    width: 70%;
+    margin: auto;
+  `};
+`;
+
+function ContactsPage({ data }) {
   return (
-    <div>
+    <div className="mh2">
       <ContentTitle>
         <FormattedMessage id="contact.title" />
       </ContentTitle>
+
+      <CurrentLocale
+        render={({ locale }) => {
+          const contact =
+            data && data[`contact_${locale}_${contentData.$key}`]
+              ? data[`contact_${locale}_${contentData.$key}`]
+              : undefined;
+          return contact ? (
+            <Contacts>
+              <ReactMarkdown source={contact.markdown.value} />
+            </Contacts>
+          ) : null;
+        }}
+      />
     </div>
   );
 }
 
+ContactsPage.propTypes = {
+  data: PropTypes.shape({}).isRequired,
+};
+
 export default ContactsPage;
+
+export const pageQuery = graphql`
+  query ContactsPageQuery {
+    contact_en_esc: contentfulMarkdown(
+      key: { eq: "esc.contacts" }
+      node_locale: { eq: "en-US" }
+    ) {
+      markdown: childContentfulMarkdownValueTextNode {
+        value
+      }
+    }
+    contact_sv_esc: contentfulMarkdown(
+      key: { eq: "esc.contacts" }
+      node_locale: { eq: "sv-SE" }
+    ) {
+      markdown: childContentfulMarkdownValueTextNode {
+        value
+      }
+    }
+    contact_en_statrem: contentfulMarkdown(
+      key: { eq: "statrem.contacts" }
+      node_locale: { eq: "en-US" }
+    ) {
+      markdown: childContentfulMarkdownValueTextNode {
+        value
+      }
+    }
+    contact_sv_statrem: contentfulMarkdown(
+      key: { eq: "statrem.contacts" }
+      node_locale: { eq: "sv-SE" }
+    ) {
+      markdown: childContentfulMarkdownValueTextNode {
+        value
+      }
+    }
+  }
+`;
