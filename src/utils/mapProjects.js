5ac3727cefb13095cd76fe45fe0a9a4e60bc3927
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-06-11 00:30:44 +0300

    fix: remove console log

--- src/utils/mapProjects.js
+++ src/utils/mapProjects.js
@@ -1,12 +1,4 @@
 const mapProjects = (data, locale) => {
-  data.edges.forEach(item => {
-    item.node.frontmatter.images.forEach(image => {
-      if (!image.image) {
-        console.log(item);
-        console.log(image.image);
-      }
-    });
-  });
   let projects = data.edges.map(item => ({
     id: item.node.id,
     title: item.node.frontmatter[`title_${locale}`],
