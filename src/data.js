c4b8f9e4814ac4b0a0ddadbc67308cab117213c1
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-06-11 23:48:11 +0300

    feat: add favicon and meta tags

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const target = process.env.GATSBY_DATA_TARGET;
 if (['esc', 'statrem'].indexOf(target) === -1) {
   throw new Error(`Invalid GATSBY_DATA_TARGET - ${JSON.stringify(target)}`);
@@ -9,8 +11,27 @@ const data = {
     url: 'http://esconstruction.eu',
     urlName: 'esconstruction.eu',
     startYear: 2017,
-    companyName: 'Eco Scandinavian Construction',
+    companyName: 'Eco Scandinavian Construction AB',
     email: 'info@esconstruction.eu',
+    metaTitle: 'Eco Scandinavian Construction AB',
+    metaDescription:
+      'Eco Scandinavian Construction is a construction company. We specialize in services such as Design/Build, Renovations and Construction Management.',
+    metaSocialElement: [
+      <meta key="1" property="og:image" content="/og-image.jpg" />,
+      <meta key="2" property="og:image:width" content="279" />,
+      <meta key="3" property="og:image:height" content="279" />,
+      <meta
+        key="4"
+        property="og:title"
+        content="Eco Scandinavian Construction AB"
+      />,
+      <meta
+        key="5"
+        property="og:description"
+        content="Eco Scandinavian Construction is a construction company. We specialize in services such as Design/Build, Renovations and Construction Management."
+      />,
+      <meta key="6" property="og:url" content="https://esconstruction.eu/" />,
+    ],
   },
   statrem: {
     $key: 'statrem',
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,6 +6,7 @@ import styled, { ThemeProvider } from 'styled-components';
 import { TranslateProvider } from '../i18n/components';
 import { Header, Footer } from '../components';
 import { theme } from '../style';
+import data from '../data';
 
 const Container = styled.div`
   min-height: 100vh;
@@ -22,20 +23,34 @@ function Wrapper({ children, location: { pathname } }) {
     <ThemeProvider theme={theme}>
       <TranslateProvider>
         <Container>
-          <Helmet
-            title="ESC"
-            meta={[
-              { name: 'description', content: 'ESC' },
-              { name: 'keywords', content: 'ESC' },
-            ]}
-          >
+          <Helmet>
             <html lang="en" />
+            <title>{data.metaTitle}</title>
+            <meta name="description" content={data.metaDescription} />
             <meta httpEquiv="Cache-control" content="public" />
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta
               name="google-site-verification"
               content="NWrrGB31y_lYtD7snlWTGiULuYIey4CuzkTsBw-uwKw"
             />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            {data.metaSocialElement}
           </Helmet>
           <Header pathname={pathname} />
           <Content>
