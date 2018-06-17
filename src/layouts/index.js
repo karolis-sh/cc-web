c11b0ac8371ec6573ccde85c08ac64839f3df60f
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-06-17 18:55:36 +0300

    fix: webfont loading

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -19,51 +19,59 @@ const Content = styled.div`
   flex: 1;
 `;
 
-WebFont.load({ google: { families: ['Asap'] } });
+class Wrapper extends React.Component {
+  componentDidMount() {
+    WebFont.load({ google: { families: ['Asap'] } });
+  }
 
-function Wrapper({ children, location: { pathname } }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <TranslateProvider>
-        <Container>
-          <Helmet>
-            <html lang="en" />
-            <title>{data.metaTitle}</title>
-            <meta name="description" content={data.metaDescription} />
-            <meta httpEquiv="Cache-control" content="public" />
-            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <meta
-              name="google-site-verification"
-              content="NWrrGB31y_lYtD7snlWTGiULuYIey4CuzkTsBw-uwKw"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/favicon-16x16.png"
-            />
-            {data.metaSocialElement}
-          </Helmet>
-          <Header pathname={pathname} />
-          <Content>
-            <div className="container ph2">{children()}</div>
-          </Content>
-          <Footer />
-        </Container>
-      </TranslateProvider>
-    </ThemeProvider>
-  );
+  render() {
+    const {
+      children,
+      location: { pathname },
+    } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <TranslateProvider>
+          <Container>
+            <Helmet>
+              <html lang="en" />
+              <title>{data.metaTitle}</title>
+              <meta name="description" content={data.metaDescription} />
+              <meta httpEquiv="Cache-control" content="public" />
+              <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+              <meta
+                name="google-site-verification"
+                content="NWrrGB31y_lYtD7snlWTGiULuYIey4CuzkTsBw-uwKw"
+              />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
+              {data.metaSocialElement}
+            </Helmet>
+            <Header pathname={pathname} />
+            <Content>
+              <div className="container ph2">{children()}</div>
+            </Content>
+            <Footer />
+          </Container>
+        </TranslateProvider>
+      </ThemeProvider>
+    );
+  }
 }
 
 Wrapper.propTypes = {
