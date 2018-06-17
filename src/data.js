059f33ea3c9b4947ddd21e0619570fc9eda4326d
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-06-17 18:34:37 +0300

    feat: update og metatags

--- src/data.js
+++ src/data.js
@@ -17,21 +17,20 @@ const data = {
     metaDescription:
       'Eco Scandinavian Construction is a construction company. We specialize in services such as Design/Build, Renovations and Construction Management.',
     metaSocialElement: [
-      <meta key="1" property="og:image" content="/og-image.jpg" />,
-      <meta key="2" property="og:image:width" content="279" />,
-      <meta key="3" property="og:image:height" content="279" />,
-      <meta
-        key="4"
-        property="og:title"
-        content="Eco Scandinavian Construction AB"
-      />,
-      <meta
-        key="5"
-        property="og:description"
-        content="Eco Scandinavian Construction is a construction company. We specialize in services such as Design/Build, Renovations and Construction Management."
-      />,
-      <meta key="6" property="og:url" content="https://esconstruction.eu/" />,
-    ],
+      { property: 'title', content: 'Eco Scandinavian Construction AB' },
+      {
+        property: 'description',
+        content:
+          'Eco Scandinavian Construction is a construction company. We specialize in services such as Design/Build, Renovations and Construction Management.',
+      },
+      { property: 'url', content: 'https://esconstruction.eu/' },
+      { property: 'type', content: 'website' },
+      { property: 'image', content: '/og-image.jpg' },
+      { property: 'image:width', content: '279' },
+      { property: 'image:height', content: '279' },
+    ].map(({ property, ...props }) => (
+      <meta key={property} property={`og:${property}`} {...props} />
+    )),
   },
   statrem: {
     $key: 'statrem',
